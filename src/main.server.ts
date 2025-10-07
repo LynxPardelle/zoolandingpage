c41a994e1665133f32f4018a78f4e3df4ec257b9
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2025-10-07 01:37:20 -0600

    Upgrade Angular and build dependencies to 20.3.x
    Updated Angular core, CLI, Material, and related packages to version 20.3.x in package.json and package-lock.json. Also replaced 'rolldown' with 'rollup' and updated related build dependencies for improved compatibility and support.

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { config } from './app/app.config.server';
 import { AppShellComponent } from './app/core/components/layout';
 
-const bootstrap = () => bootstrapApplication(AppShellComponent, config);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppShellComponent, config, context);
 
 export default bootstrap;
