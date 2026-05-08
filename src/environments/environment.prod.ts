5eee3a7acc4ea1be8017455d830477eb2923c036
Lynx Pardelle <lnxdrk@gmail.com>
2026-05-07 20:02:42 -0600

    Use raw API proxy in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,7 @@ export const environment: TEnvironment = {
   production: true,
   development: false,
   apiUrl: 'https://api.zoolandingpage.com.mx',
+  apiProxyUrl: 'https://yxp97qlog2.execute-api.us-east-1.amazonaws.com/Prod',
   configApiUrl: 'https://api.zoolandingpage.com.mx',
   configApiRuntimeFallbackUrl: 'https://y84vk0v44l.execute-api.us-east-1.amazonaws.com/Prod',
   configApiServerFallbackUrl: 'https://y84vk0v44l.execute-api.us-east-1.amazonaws.com/Prod',
